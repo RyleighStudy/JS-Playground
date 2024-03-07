bb5cff2cf2a2b8a88ef21cf596a57c967336e549
RyleighStudy <riley-congdon@live.nmit.ac.nz>
2024-03-07 13:50:52 +1300

    added number.js

---
+++ number.js
@@ -0,0 +1,10 @@
+const num1 = 10
+console.log(num1)
+//NOTE - can't be updated or redifined, block scoped 
+let num2 = 20
+console.log(num2)
+//NOTE - can be updated but not redifined, block scoped
+var num3 = 30
+console.log(num3)
+//NOTE - can be updated and redifined, not block scoped
+
